9e4b34f22a65445662a882ec0b3d418e17b579b2
Nikita Belevich <belevish_1998@mail.ru>
2021-03-31 23:48:27 +0300

    feat: Добавлен класс активности для пункта меню в соответствии с текущей страницей

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 'use strict';
 
 
-console.warn('Hello, I am client JS');
+// console.warn('Hello, I am client JS');
+
+addActiveClassOnMenuItem();
+function addActiveClassOnMenuItem() {
+    const regexp1 = /[\w\-]+(?=\.html)/;
+    const path = document.location.pathname;
+    const nameOfCurrentPage = (path == '/') ? 'index' : path.match(regexp1)[0];
+    const currentMenuItem = document.querySelector(`[data-page-name="${nameOfCurrentPage}"]`);
+    currentMenuItem.classList.add('header-of-site__menu-item_active');
+    // console.log(currentMenuItem);
+}
